b002dcdbe24f3bd2a602652f01911abe9bcaac80
Ryan Hirsch <ryan.hirsch@gmail.com>
2019-03-22 17:40:16 -0500

    steps function

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,71 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = '') {
+  if(n === row) {
+    return;
+  }
+
+  if(stair.length === n) {
+    console.log(stair);
+    return steps(n, row + 1)
+  }
+
+  const char = stair.length <= row ? "#" : " "
+  return steps(n, row, stair + char)
+}
 
 module.exports = steps;
+
+// function steps(n, row = 0, stair = '') {
+//   if(n === row) {
+//     return;
+//   }
+
+//   if(stair.length === n) {
+//     console.log(stair);
+//     return steps(n, row + 1)
+//   }
+//   if(stair.length <= row) {
+//     return steps(n, row, stair + "#")
+//   }
+//   return steps(n, row, stair + " ")
+// }
+
+
+// function steps(n) {
+//   let steps = Array(n).fill(" ");
+//   for(let i = 0; i < n; i++) {
+//     steps[i] = "#";
+//     console.log(steps.join(''))
+//   }
+// }
+
+// function steps(n) {
+//   for(let row = 0; row < n; row++) {
+//     let step = "";
+//     for(let col = 0; col < n; col++) {
+//       if(col <= row) {
+//         step += "#";
+//       }
+//       else {
+//         step += " ";
+//       }
+//     }
+//     console.log(step)
+//   }
+// }
+
+// function steps(n) {
+//   for(let i = 0; i < n; i++) {
+//     console.log(`${"#".repeat(i + 1)}${" ".repeat(n - i - 1)}`)
+//   }
+// }
+
+// function steps(n) {
+//   let steps = `${" ".repeat(n)}`.split('');
+//   for(let i = 0; i < n; i++) {
+//     steps[i] = "#";
+//     console.log(steps.join(''))
+//   }
+// }
