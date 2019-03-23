6c896f5a18d7770bc9e6dcc4b323297076abc13f
Ryan Hirsch <ryan.hirsch@gmail.com>
2019-03-23 15:08:39 -0500

    solve spiral matrix with a FSM

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,65 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const final = [];
+  for(let i = 0; i < n; i++) {
+    final.push(new Array(n));
+  }
+  let tr = n - 1; //column
+  let tl = 1; // row
+  let br = n - 1; // row
+  let bl = 0; // column
+  let col = 0;
+  let row = 0;
+  let dir = 'r';
+
+  for(let i = 1; i <= n * n; i++) {
+    final[row][col] = i;
+    switch(dir) {
+      case "r":
+        if(col + 1 <= tr) { // col
+          col++;
+        }
+        else {
+          dir = "d";
+          row++;
+          tr--;
+        }
+        break;
+      case "l":
+        if(col - 1 >= bl) { // col
+          col--;
+        }
+        else {
+          dir = "u";
+          row--;
+          bl++;
+        }
+        break;
+      case "d":
+        if(row + 1 <= br) { // row
+          row++;
+        }
+        else {
+          dir = "l";
+          col--;
+          br--;
+        }
+        break;
+      case "u":
+      if(row - 1 >= tl) { // row
+        row--;
+      }
+      else {
+        dir = "r";
+        col++;
+        tl++;
+      }
+      break;
+    }
+  }
+  return final;
+}
 
 module.exports = matrix;
