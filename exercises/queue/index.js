0c2c25ab91c9a037f7a95e4edc2ea3100ad6c6d4
Ryan Hirsch <ryan.hirsch@gmail.com>
2019-03-23 16:32:08 -0500

    implement a queue

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,18 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.queue = [];
+  }
+
+  add(item) {
+    this.queue.push(item)
+  }
+
+  remove() {
+    return this.queue.shift()
+  }
+}
 
 module.exports = Queue;
