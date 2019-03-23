ed8dab22e20c8b32e2a8dacfbb9bdff64e88933f
Ryan Hirsch <ryan.hirsch@gmail.com>
2019-03-23 16:55:22 -0500

    stack

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -10,6 +10,22 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+  constructor() {
+    this.data = [];
+  }
+
+  push(item) {
+    this.data.push(item);
+  }
+
+  pop() {
+    return this.data.pop();
+  }
+
+  peek() {
+    return this.data[this.data.length - 1];
+  }
+}
 
 module.exports = Stack;
